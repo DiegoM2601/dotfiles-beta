d78eef9cd791274b290e3b883defe82ec8b27dfb
Diego Mendoza <diegom.mendoza2601@gmail.com>
2026-05-04 19:57:38 -0400

    04-05-2026

---
+++ .dwm/autostart.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+picom --daemon &
+lxpolkit &
+thunar --daemon &
+dunst &
+nm-applet &
+dwmblocks &
+setxkbmap -layout us,latam -option grp:win_space_toggle &
+feh --bg-fill ~/Pictures/fondo1.jpg &
---
+++ dwmblocks/blocks.h
@@ -0,0 +1,10 @@
+static const Block blocks[] = {
+/*Icon*/  /*Command*/       /*Interval*/  /*Signal*/
+{"",      "net-speed",      2,            0},
+{"",      "vol-status",     2,            10},
+{"",      "bat-status",    30,             0},
+{"",      "date '+󰃰 %d/%m/%Y 󰥔 %H:%M'", 60, 0},
+};
+
+static char delim[] = "  ";
+static unsigned int delimLen = 2;
